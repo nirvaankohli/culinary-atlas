f8acf8e473d2420214f14a5029e1a74b85e90188
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-12-02 13:51:00 -0500

    Enhance recipe scraper functionality: add API key cycling, load environment variables, and update common settings; remove example response file

--- agents/scrapers/recipes/client.py
+++ agents/scrapers/recipes/client.py
@@ -1,12 +1,19 @@
 import json
 import requests
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 ROOT_PATH = Path(__file__).parent.parent.parent.parent
 SETTING_PATH = ROOT_PATH / "agents" / "common_settings.json"
 EXAMPLE_PATH = ROOT_PATH / "agents" / "examples"
 EXAMPLE_OUTPUT_PATH = EXAMPLE_PATH / "scrapers_recipes_output.json"
 INPUT_PATH = EXAMPLE_PATH / "diversifier_output.json"
+ENV_PATH = json.loads(open(SETTING_PATH, "r", encoding="utf-8").read())["env"]["path"]
+KEY_PATH = Path(__file__).parent / "current_key_num.json"
+NUM_API_KEYS = 2
+
+load_dotenv(ENV_PATH)
 
 
 def clear_of_junk(text: str) -> str:
@@ -20,20 +27,55 @@ def clear_of_junk(text: str) -> str:
     )
 
 
-def process(search_term: str):
+def get_current_key_num() -> int:
+
+    with open(KEY_PATH, "r", encoding="utf-8") as f:
+
+        key_data = json.load(f)
+
+    return key_data.get("current_key_num", 0)
+
+
+def cycle_key(num_keys: int = NUM_API_KEYS) -> None:
+
+    current_key_num = get_current_key_num()
+
+    new_key_num = (current_key_num + 1) % num_keys
+    with open(KEY_PATH, "w", encoding="utf-8") as f:
+
+        json.dump({"current_key_num": new_key_num}, f)
+
+
+def process(
+    search_term: str, other_params: dict = {}, recursion_depth: int = 0
+) -> dict:
 
     search_term = clear_of_junk(search_term)
 
-    print(search_term)
+    params = other_params.copy()
+    params["query"] = search_term
 
-    url = f"https://www.themealdb.com/api/json/v1/1/search.php?s={search_term}"
+    current_key_num = get_current_key_num()
 
-    response = requests.get(url)
+    API_KEY = os.getenv(f"RECIPE_API_KEY_{current_key_num}")
+    params["apiKey"] = API_KEY
 
-    if response.status_code != 200:
-        raise Exception(f"API request failed with status code {response.status_code}")
-    data = response.json()
-    return data
+    url = "https://api.spoonacular.com/recipes/complexSearch"
+
+    results = requests.get(
+        url,
+        params=params,
+    )
+
+    if results.status_code == 429 and recursion_depth < NUM_API_KEYS - 1:
+
+        cycle_key()
+
+    elif results.status_code != 200:
+
+        return {"status_code": results.status_code}
+
+    return results.json()
 
 
 if __name__ == "__main__":
@@ -43,6 +85,7 @@ if __name__ == "__main__":
         input_data = json.load(f)
 
     response = process(input_data[0]["recipe_search_prompt"])
+    print(response)
 
     with open(EXAMPLE_OUTPUT_PATH, "w", encoding="utf-8") as f:
 
